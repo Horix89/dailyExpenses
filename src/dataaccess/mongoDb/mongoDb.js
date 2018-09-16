4e7ba4853bb156cd9a9b77786af9e455aefa0e49
Horacio Brizuela <horaciob@lagash.com>
2018-09-16 14:00:10 -0300

    add package.json and create docker-compose.yml

--- src/dataaccess/mongoDb/mongoDb.js
+++ src/dataaccess/mongoDb/mongoDb.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose'), Schema = mongoose.Schema;;
-const DATABASE_CONNECTION = 'mongodb://localhost/test';
+const DATABASE_CONNECTION = 'mongodb://mongo/dailyExpenses';
 
 var ExpenseSchema = new Schema({
     description   : { type: String, required: true }
   , price     : { type: Number,  required: true }
 });
 
-const Expense = mongoose.model('Expensives', ExpenseSchema);
+const Expense = mongoose.model('Expenses', ExpenseSchema);
 
 exports.initializeMongo = function(){
     mongoose.connect(DATABASE_CONNECTION);
